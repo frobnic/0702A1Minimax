41a1b2a5d250e4bb1477f18133faa3f4d9d0f54c
frank paulsen <frobnic+ita@gmail.com>
2019-08-29 09:46:27 +0200

    code inserted

--- 0702A1Minimax/Program.cs
+++ 0702A1Minimax/Program.cs
@@ -8,8 +8,32 @@ namespace _0702A1Minimax
 {
     class Program
     {
+        static int Min(int a, int b)
+        {
+            if (a > b)
+                return b;
+            else
+                return a;
+        }
+
+        static int Max(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+
         static void Main(string[] args)
         {
+            int a, b;
+
+            Console.Write("Bitte Zahl eins eingeben:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Bitte Zahl zwei eingeben:");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Die grössere Zahl ist {0}, die kleinere Zahl ist {1}", Max(a, b), Min(a, b));
         }
     }
 }
